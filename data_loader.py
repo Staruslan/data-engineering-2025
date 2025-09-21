4bb2a5fc9c85bc9ad76154b13cb20ba7f938cc7e
Ruslan K <67726937+Staruslan@users.noreply.github.com>
2025-09-21 16:45:34 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 FILE_ID = "1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq"  # ID файла на Google Drive
-file_url = f"https://drive.google.com/file/d/1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq/view?usp=drive_link={FILE_ID}"
+file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 
-raw_data = pd.read_csv(file_url)     # читаем файл
-print(raw_data.head(10))         # выводим на экран первые 10 строк для проверки
+raw_data = pd.read_csv(file_url, on_bad_lines='skip') # Читаем CSV-файл с Google Диска
+print(raw_data.head(10)) #вывод 
