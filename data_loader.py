9e9946a622912e9d88327f3c7bf3e789453d04cc
Ruslan K <67726937+Staruslan@users.noreply.github.com>
2025-09-21 15:37:16 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,7 @@
 import pandas as pd
-import requests
-from io import StringIO
-#https://drive.google.com/file/d/1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq/view?usp=drive_link
 
-FILE_ID = "1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq"
+FILE_ID = "1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq"  # ID файла на Google Drive
 file_url = f"https://drive.google.com/file/d/1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq/view?usp=drive_link={FILE_ID}"
 
-response = requests.get(file_url)
-raw_data = pd.read_csv(StringIO(response.text))
-print(raw_data.head(10))
+raw_data = pd.read_csv(file_url)     # читаем файл
+print(raw_data.head(10))         # выводим на экран первые 10 строк для проверки
