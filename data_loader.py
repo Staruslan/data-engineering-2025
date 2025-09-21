23ab468cd59ed51af37fd8c11c909dc6e8523b99
Ruslan K <67726937+Staruslan@users.noreply.github.com>
2025-09-21 14:14:32 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import requests
+from io import StringIO
+#https://drive.google.com/file/d/1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq/view?usp=drive_link
 
+FILE_ID = "1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq"
+file_url = f"https://drive.google.com/file/d/1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq/view?usp=drive_link={FILE_ID}"
+
+response = requests.get(file_url)
+raw_data = pd.read_csv(StringIO(response.text))
+print(raw_data.head(10))
