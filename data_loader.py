9a15243e41bc7bfa27cad49b123896fc4b3ed9fc
Ruslan K <67726937+Staruslan@users.noreply.github.com>
2025-09-30 01:17:35 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,12 +1,4 @@
 import pandas as pd
-
-FILE_ID = "1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq"  # ID файла на Google Drive
-file_url = f"https://drive.google.com/uc?id={FILE_ID}"
-
-raw_data = pd.read_csv(file_url, on_bad_lines='skip') # Читаем CSV-файл с Google Диска
-print(raw_data.head(10)) #вывод 
-
-import pandas as pd
 import requests
 from io import StringIO
 
