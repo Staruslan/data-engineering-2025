8625d4f76a27e7ad402e9a99e6aa1790cb1ad924
Ruslan K <67726937+Staruslan@users.noreply.github.com>
2025-09-30 00:29:12 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -5,3 +5,26 @@ file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 
 raw_data = pd.read_csv(file_url, on_bad_lines='skip') # Читаем CSV-файл с Google Диска
 print(raw_data.head(10)) #вывод 
+
+
+import pandas as pd
+import requests
+from io import StringIO
+
+url='https://drive.google.com/file/d/1xxVEprqDIAMIc9rAIo5LNjAjAzfH2sPq/view?usp=share_link'
+
+file_id = url.split('/')[-2]
+dwn_url='https://drive.google.com/uc?export=download&id=' + file_id
+url2 = requests.get(dwn_url).text
+csv_raw = StringIO(url2)
+raw_data = pd.read_csv(csv_raw)
+
+# print('Переменные:',raw_data.columns)
+
+# достаем имя переменных
+size_perm = len(list(raw_data.columns)) # количество переменных в таблице
+print('Количество переменных',size_perm)
+
+
+for i in range(0, size_perm - 1, 1):
+    print('Переменная',list(raw_data)[i], 'Тип переменной', (raw_data[list(raw_data)[i]].values).dtype)
