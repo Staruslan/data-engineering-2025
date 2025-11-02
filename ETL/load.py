096b6fc96b4963ed1f1c9d6ae94624a227815ebf
Ruslan K <67726937+Staruslan@users.noreply.github.com>
2025-11-02 22:15:02 +0300

    Update load.py

--- ETL/load.py
+++ ETL/load.py
@@ -1,48 +1,53 @@
 import os
 import pandas as pd
 from sqlalchemy import create_engine, inspect, text
-#from dotenv import load_dotenv
+# from dotenv import load_dotenv
+
 
 def create_engine_and_inspector():
     load_dotenv()
-    db_user = os.getenv('DB_USER')
-    db_password = os.getenv('DB_PASSWORD')
-    db_url = os.getenv('DB_URL')
-    db_port = os.getenv('DB_PORT')
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_url = os.getenv("DB_URL")
+    db_port = os.getenv("DB_PORT")
     db_name = "homeworks"
-    
+
     engine = create_engine(
         f"postgresql+psycopg2://{db_user}:{db_password}@{db_url}:{db_port}/{db_name}"
     )
-    
+
     inspector = inspect(engine)
     return engine, inspector
 
+
 def save_to_parquet(df, parquet_path="data/processed/output.parquet"):
-    df.to_parquet(parquet_path, engine='pyarrow', index=False)
+    df.to_parquet(parquet_path, engine="pyarrow", index=False)
     print(f"Data saved to Parquet at {parquet_path}")
+
 
 def load_data_to_postgres(df, engine):
     table_name = "komarov"
     with engine.connect() as conn:
         print("Successfully connected to PostgreSQL")
-        
+
     df.to_sql(
         name=table_name,
         con=engine,
         schema="public",
         if_exists="replace",
-        index=True
+        index=True,
     )
     print(f"Data loaded into PostgreSQL table: {table_name}")
 
     with engine.begin() as conn:
-        conn.execute(text(f'ALTER TABLE public.{table_name} ADD PRIMARY KEY (index)'))
+        conn.execute(text(f"ALTER TABLE public.{table_name} ADD PRIMARY KEY (index)"))
+
 
 def print_table_structure(inspector, table_name="komarov"):
     columns = inspector.get_columns(table_name, schema="public")
     print("\nTable structure:")
     print({col["name"]: col["type"] for col in columns})
+
 
 def load(df: pd.DataFrame, parquet_path: str = "data/processed/output.parquet"):
     """Load data to PostgresSQL and save as Parquet."""
@@ -50,4 +55,3 @@ def load(df: pd.DataFrame, parquet_path: str = "data/processed/output.parquet"):
     engine, inspector = create_engine_and_inspector()
     load_data_to_postgres(df, engine)
     print_table_structure(inspector)
-
