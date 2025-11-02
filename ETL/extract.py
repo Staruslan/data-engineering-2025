0597fd3abfe02b6a4aa4c4e5237044a7471fb9b7
Ruslan K <67726937+Staruslan@users.noreply.github.com>
2025-11-02 22:13:55 +0300

    Update extract.py

--- ETL/extract.py
+++ ETL/extract.py
@@ -6,8 +6,8 @@ import os
 
 def extract(url: str, save_path: str = "data/raw/raw_data.csv") -> pd.DataFrame:
     """Download file from Google Drive and save as CSV."""
-    file_id = url.split('/')[-2]
-    dwn_url = f'https://drive.google.com/uc?export=download&id={file_id}'
+    file_id = url.split("/")[-2]
+    dwn_url = f"https://drive.google.com/uc?export=download&id={file_id}"
     response = requests.get(dwn_url).text
     csv_raw = StringIO(response)
     df = pd.read_csv(csv_raw)
